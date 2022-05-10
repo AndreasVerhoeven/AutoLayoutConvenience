2c4fdb6a81ce749ba3f5640c835376d1c28dc623
Andreas Verhoeven <ave@aveapps.com>
2022-05-10 18:01:15 +0200

    Added AutoSizingTableView and AutoSizingCollectionView

---
+++ Sources/Other/AutoSizingCollectionView.swift
@@ -0,0 +1,19 @@
+//
+//  AutoSizingCollectionView.swift
+//  AutoLayoutConvenience
+//
+//  Created by Andreas Verhoeven on 10/05/2022.
+//
+
+import UIKit
+
+/// A UICollectionView with its intrinsic content size the same as the contentSize
+class AutoSizingCollectionView: UICollectionView {
+	public override var contentSize: CGSize {
+		didSet {
+			guard contentSize != oldValue else { return }
+			invalidateIntrinsicContentSize()
+		}
+	}
+	public override var intrinsicContentSize: CGSize { contentSize }
+}
---
+++ Sources/Other/AutoSizingTableView.swift
@@ -0,0 +1,19 @@
+//
+//  AutoSizingTableView.swift
+//  AutoLayoutConvenience
+//
+//  Created by Andreas Verhoeven on 10/05/2022.
+//
+
+import UIKit
+
+/// A UITableView with its intrinsic content size the same as the contentSize
+class AutoSizingTableView: UITableView {
+	public override var contentSize: CGSize {
+		didSet {
+			guard contentSize != oldValue else { return }
+			invalidateIntrinsicContentSize()
+		}
+	}
+	public override var intrinsicContentSize: CGSize { contentSize }
+}
